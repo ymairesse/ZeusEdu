fcf5618720d0d7bc5ab3ccd82071d1416309f72a
ymairesse <ymairesse@sio2.be>
2013-12-21 15:45:14 +0100

    Ajout du répertoire 'photos'

---
+++ photos/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once ("../inc/fonctions.inc.php");
+if (!(accesApplication(APPLICATION))) 
+	header ("Location: ../accueil.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta content="text/html; charset=UTF-8" http-equiv="content-type">
+<title></title>
+</head>
+<body>
+</body>
+</html>
