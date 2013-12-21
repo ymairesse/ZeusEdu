ba2453c54f3ce2f80ad041dfa6acaa8e8f243786
ymairesse <ymairesse@sio2.be>
2013-12-21 14:10:27 +0100

    Ajout des fichiers non .php

---
+++ javascript.js
@@ -0,0 +1,18 @@
+<script type="text/javascript" src="../js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="../js/jquery-ui-1.10.3/ui/minified/jquery-ui.min.js"></script>
+<!--
+<script type="text/javascript" src="../js/jquery.form.js"></script>
+-->
+<script type="text/javascript" src="../js/jquery.validate.js"></script>
+<script type="text/javascript" src="../js/jquery.blockUI.js"></script>
+<script type="text/javascript" src="../js/jquery.enter2tab.js"></script>
+<script type="text/javascript" src="../js/toTop/jquery.ui.totop.js"></script>
+<script type="text/javascript" src="../js/menuBas.js"></script>
+<script type="text/javascript" src="../js/timepicker/jquery.ui.timepicker.js"></script>
+<script type="text/javascript" src="../js/toTop/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="../js/jquery.ymtooltip.js"></script>
+<script type="text/javascript" src="../js/jquery.align.js"></script>
+
+<script type="text/javascript">
+	$.blockUI.defaults.message = 'Veuillez patienter';
+</script>
