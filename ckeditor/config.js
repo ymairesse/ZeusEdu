974e738d4c197d9e717fdb223422e180776b0002
ymairesse <ymairesse@sio2.be>
2014-01-25 18:05:04 +0100

    Mise à jour de ckeditor 4.1 > 4.3

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -1,44 +1,75 @@
 /**
  * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
-
 	// For the complete reference:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection'] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
+	
+	// Remove some buttons, provided by the standard plugins, which we don't
+	// need to have in the Standard(s) toolbar.
+	// config.removeButtons = 'Underline,Subscript,Superscript';
 
-
-
-	// Se the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Make dialogs simpler.
 	// config.removeDialogTabs = 'image:advanced;link:advanced';
 
+	// Se the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
 	config.height = '400px';
 
 	config.disableNativeSpellChecker = false;
+	
+	config.scayt_autoStartup = true; // Pour activer le correcteur orthographique
+    config.scayt_sLang = 'fr_FR'; // Correcteur orthographique en Français
 
 	config.extraPlugins = 'print,font';
+
+	config.skin = 'moono';
+	
+	
+	// Toolbar configuration generated automatically by the editor based on config.toolbarGroups.
+config.toolbar = [
+	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll'] },
+	
+	'/',
+	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align'], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-',  'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+	{ name: 'links', items: [ 'Link', 'Unlink' ] },
+	{ name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'Smiley' ] },
+	'/',
+	{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+	{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+	{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+	{ name: 'others', items: [ '-' ] },
+	{ name: 'about', items: [ 'About' ] }
+];
+
+// Toolbar groups configuration.
+config.toolbarGroups = [
+	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
+	{ name: 'forms' },
+	'/',
+	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+	{ name: 'links' },
+	{ name: 'insert' },
+	'/',
+	{ name: 'styles' },
+	{ name: 'colors' },
+	{ name: 'tools' },
+	{ name: 'others' },
+	{ name: 'about' }
+];
+	
+	
 
 };
