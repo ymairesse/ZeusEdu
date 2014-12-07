f06e7f64dc737661f07cd1d6d7644810e044ee7d
ymairesse <ymairesse@sio2.be>
2014-12-07 19:16:14 +0100

    Mise à jour de ckeditor

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -1,75 +1,40 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
-	// For the complete reference:
+	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	
-	// Remove some buttons, provided by the standard plugins, which we don't
-	// need to have in the Standard(s) toolbar.
-	// config.removeButtons = 'Underline,Subscript,Superscript';
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
 
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
 
-	// Make dialogs simpler.
-	// config.removeDialogTabs = 'image:advanced;link:advanced';
-
-	// Se the most common block elements.
+	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
-	config.height = '400px';
-
-	config.disableNativeSpellChecker = false;
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 	
-	config.scayt_autoStartup = true; // Pour activer le correcteur orthographique
-    config.scayt_sLang = 'fr_FR'; // Correcteur orthographique en Français
-
-	config.extraPlugins = 'print,font';
-
-	config.skin = 'moono';
-	
-	
-	// Toolbar configuration generated automatically by the editor based on config.toolbarGroups.
-config.toolbar = [
-	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
-	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll'] },
-	
-	'/',
-	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align'], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-',  'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
-	{ name: 'links', items: [ 'Link', 'Unlink' ] },
-	{ name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'Smiley' ] },
-	'/',
-	{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-	{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-	{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
-	{ name: 'others', items: [ '-' ] },
-	{ name: 'about', items: [ 'About' ] }
-];
-
-// Toolbar groups configuration.
-config.toolbarGroups = [
-	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
-	{ name: 'forms' },
-	'/',
-	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-	{ name: 'links' },
-	{ name: 'insert' },
-	'/',
-	{ name: 'styles' },
-	{ name: 'colors' },
-	{ name: 'tools' },
-	{ name: 'others' },
-	{ name: 'about' }
-];
-	
-	
-
+	config.height = 600;
 };
