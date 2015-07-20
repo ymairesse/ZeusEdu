2d13aaf4dee6fe207c2e70d3d7e0b53a47045f3c
Yves Mairesse <ymairesse@sio2.be>
2015-07-20 15:09:03 +0200

    Modification insignifiante

--- hermes/index.php
+++ hermes/index.php
@@ -28,12 +28,17 @@ switch ($action) {
 		$mailExpediteur = $_POST['mailExpediteur'];
 		$_POST['nomExpediteur'] = ($mailExpediteur == NOREPLY)?NOMNOREPLY:$_POST['nomExpediteur'];
 
+		// création du groupe éventuellement créé durant l'envoi du mail
 		$hermes->archiveMail($acronyme, $_POST, $_FILES);
 		if ($_POST['groupe'] != '') {
 			$hermes->creerGroupe($acronyme,$_POST['groupe'],$_POST['mails']);
 			}
 
+		// la liste des destinataires est dans $_POST sous forme mail#nom
+		// la liste des fichiers joints est dans $_FILES
 		$smarty->assign('detailsMail', array('post'=>$_POST,'files'=>$_FILES));
+		// éclater la liste des destinataires sur le #
+		// chaque destinataire est de la forme  "Prenom Nom#pnom@ecole.org"
 		$destinataires = $hermes->listeNomsFromDestinataires($_POST['mails']);
 		$smarty->assign('destinatairesString', implode(', ',$destinataires));
 		$smarty->assign('corpsPage','confirmMail');
