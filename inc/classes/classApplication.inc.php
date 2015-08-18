94052d2c6d61b46e737fb5318df6ed0b1788aae8
Yves Mairesse <ymairesse@sio2.be>
2015-08-18 15:45:17 +0200

    Problèmes en cas d'absence de la fonction finfo_open

--- inc/classes/classApplication.inc.php
+++ inc/classes/classApplication.inc.php
@@ -1416,84 +1416,86 @@ class Application {
 	* @param $filename: le nom du fichier
 	* @return string
 	*/
-    function checkFileType($filename) {
-
-        $mime_types = array(
-            'txt' => 'text/plain',
-            'htm' => 'text/html',
-            'html' => 'text/html',
-            'php' => 'text/html',
-            'css' => 'text/css',
-            'js' => 'application/javascript',
-            'json' => 'application/json',
-            'xml' => 'application/xml',
-            'swf' => 'application/x-shockwave-flash',
-            'flv' => 'video/x-flv',
-
-            // images
-            'png' => 'image/png',
-            'jpe' => 'image/jpeg',
-            'jpeg' => 'image/jpeg',
-            'jpg' => 'image/jpeg',
-            'gif' => 'image/gif',
-            'bmp' => 'image/bmp',
-            'ico' => 'image/vnd.microsoft.icon',
-            'tiff' => 'image/tiff',
-            'tif' => 'image/tiff',
-            'svg' => 'image/svg+xml',
-            'svgz' => 'image/svg+xml',
-
-            // archives
-            'zip' => 'application/zip',
-            'rar' => 'application/x-rar-compressed',
-            'exe' => 'application/x-msdownload',
-            'msi' => 'application/x-msdownload',
-            'cab' => 'application/vnd.ms-cab-compressed',
-
-            // audio/video
-            'mp3' => 'audio/mpeg',
-            'qt' => 'video/quicktime',
-            'mov' => 'video/quicktime',
-
-            // adobe
-            'pdf' => 'application/pdf',
-            'psd' => 'image/vnd.adobe.photoshop',
-            'ai' => 'application/postscript',
-            'eps' => 'application/postscript',
-            'ps' => 'application/postscript',
-
-            // ms office
-            'doc' => 'application/msword',
-            'rtf' => 'application/rtf',
-            'xls' => 'application/vnd.ms-excel',
-            'ppt' => 'application/vnd.ms-powerpoint',
-
-            // open office
-            'odt' => 'application/vnd.oasis.opendocument.text',
-            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
-        	);
-
-		if (function_exists('finfo_open')) {
-			$finfo = finfo_open(FILEINFO_MIME);
-            $mimetype = finfo_file($finfo, $filename);
-            finfo_close($finfo);
-            return $mimetype;
-			}
-			else {
-				$ext = strtolower(array_pop(explode('.',$filename)));
-				return $mime_types[$ext];
-				}
-    	}
+    // function checkFileType($filename) {
+	//
+    //     $mime_types = array(
+    //         'txt' => 'text/plain',
+    //         'htm' => 'text/html',
+    //         'html' => 'text/html',
+    //         'php' => 'text/html',
+    //         'css' => 'text/css',
+    //         'js' => 'application/javascript',
+    //         'json' => 'application/json',
+    //         'xml' => 'application/xml',
+    //         'swf' => 'application/x-shockwave-flash',
+    //         'flv' => 'video/x-flv',
+	// 		'csv' => 'text/plain',
+	//
+    //         // images
+    //         'png' => 'image/png',
+    //         'jpe' => 'image/jpeg',
+    //         'jpeg' => 'image/jpeg',
+    //         'jpg' => 'image/jpeg',
+    //         'gif' => 'image/gif',
+    //         'bmp' => 'image/bmp',
+    //         'ico' => 'image/vnd.microsoft.icon',
+    //         'tiff' => 'image/tiff',
+    //         'tif' => 'image/tiff',
+    //         'svg' => 'image/svg+xml',
+    //         'svgz' => 'image/svg+xml',
+	//
+    //         // archives
+    //         'zip' => 'application/zip',
+    //         'rar' => 'application/x-rar-compressed',
+    //         'exe' => 'application/x-msdownload',
+    //         'msi' => 'application/x-msdownload',
+    //         'cab' => 'application/vnd.ms-cab-compressed',
+	//
+    //         // audio/video
+    //         'mp3' => 'audio/mpeg',
+    //         'qt' => 'video/quicktime',
+    //         'mov' => 'video/quicktime',
+	//
+    //         // adobe
+    //         'pdf' => 'application/pdf',
+    //         'psd' => 'image/vnd.adobe.photoshop',
+    //         'ai' => 'application/postscript',
+    //         'eps' => 'application/postscript',
+    //         'ps' => 'application/postscript',
+	//
+    //         // ms office
+    //         'doc' => 'application/msword',
+    //         'rtf' => 'application/rtf',
+    //         'xls' => 'application/vnd.ms-excel',
+    //         'ppt' => 'application/vnd.ms-powerpoint',
+	//
+    //         // open office
+    //         'odt' => 'application/vnd.oasis.opendocument.text',
+    //         'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+    //     	);
+	//
+	// 	if (function_exists('finfo_opens')) {
+	// 		$finfo = finfo_open(FILEINFO_MIME);
+    //         $mimetype = finfo_file($finfo, $filename);
+    //         finfo_close($finfo);
+    //         return $mimetype;
+	// 		}
+	// 		else {
+	// 			afficher($filename, true);
+	// 			$ext = strtolower(array_pop(explode('.',$filename)));
+	// 			return $mime_types[$ext];
+	// 			}
+    // 	}
 
 	/**
 	 * retourn le type MIME du fichier dont on fournit le nom
 	 * @param $file
 	 * @return string
 	 */
-	// public function checkFileType($fileName) {
-	// 	$finfo = finfo_open(FILEINFO_MIME);
-	// 	return (finfo_file($finfo, $fileName));
-	// }
+	public function checkFileType($fileName) {
+		$finfo = finfo_open(FILEINFO_MIME);
+		return (finfo_file($finfo, $fileName));
+	}
 
 	/**
 	 * renvoie le résultat de la requête SQL sous forme d'array
