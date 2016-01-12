46267a88a51f0878203864e366efef68d2a603e8
Yves Mairesse <ymairesse@sio2.be>
2016-01-12 08:06:11 +0100

    Simplification

--- photos/index.php
+++ photos/index.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
-<head>
-<meta content="text/html; charset=UTF-8" http-equiv="content-type">
-<title></title>
-</head>
-<body>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+    </body>
 </html>
