c50d9de9a2a7cc0f2cf8b31c2141071b96d481eb
Yves Mairesse <ymairesse@sio2.be>
2016-06-13 18:18:07 +0200

    Mise à jour de ckeditor 4

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -28,12 +28,13 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Subscript,Superscript';
+	config.removeButtons = 'Underline,Subscript,Superscript,SpecialChar';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-	
+
+	config.extraPlugins = 'youtube';
 };
