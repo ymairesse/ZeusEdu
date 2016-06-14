1db0201bf5210891538a5dde7b67020c8cb97814
Yves Mairesse <ymairesse@sio2.be>
2016-06-14 15:48:43 +0200

    Ajout des infos sur la page d'accueil du bulletin

--- bullISND/inc/news.php
+++ bullISND/inc/news.php
@@ -1,7 +1,37 @@
 <?php
-require_once (INSTALL_DIR."/inc/classes/classFlashInfo.inc.php");
+
+$smarty->assign('PERIODESDELIBES', explode(',', PERIODESDELIBES));
+$smarty->assign('PERIODEENCOURS', PERIODEENCOURS);
+
+$listeCoursGrpProf = $user->listeCoursProf();
+
+// liste de tous les élèves avec leurs cours
+$listeCoursGrpEleves = $Ecole->listeElevesDeListeCoursGrp($listeCoursGrpProf);
+// liste de tous les élèves dont le bulletin contient une situation pour la période en cours
+$listeSituationsOK = $Bulletin->listeSituationsOK(array_keys($listeCoursGrpProf), PERIODEENCOURS);
+// on retire de la liste des élèves tout ceux qui sont OK
+foreach ($listeCoursGrpEleves as $coursGrp => $listeEleves) {
+    foreach ($listeSituationsOK as $coursGrpOK => $dataEleves) {
+        foreach ($dataEleves as $matricule => $wtf) {
+            // on supprime tous les élèves qui figurent dans la table OK
+            unset($listeCoursGrpEleves[$coursGrpOK][$matricule]);
+        }
+    }
+    // s'il n'y a plus d'élément dans la liste, on la supprime
+    if (count($listeCoursGrpEleves[$coursGrp]) == 0) {
+        unset($listeCoursGrpEleves[$coursGrp]);
+    }
+}
+$smarty->assign('listeSituationsVides',$listeCoursGrpEleves);
+
+$listeDelibeVide = $Bulletin->listeSitDelibeVides(array_keys($listeCoursGrpProf), PERIODEENCOURS);
+$smarty->assign('listeDelibeVide', $listeDelibeVide);
+
+$listeEchecNonCommentes = $Bulletin->listeEchecNonCommentes(array_keys($listeCoursGrpProf), PERIODEENCOURS);
+$smarty->assign('listeEchecNonCommentes', $listeEchecNonCommentes);
+
+require_once INSTALL_DIR.'/inc/classes/classFlashInfo.inc.php';
 $application = Application::repertoireActuel();
-$smarty->assign('application',$application);
-$smarty->assign('flashInfos', flashInfo::listeFlashInfos ($application));
-$smarty->assign('corpsPage', 'news');
-?>
+$smarty->assign('application', $application);
+$smarty->assign('flashInfos', flashInfo::listeFlashInfos($application));
+$smarty->assign('corpsPage', 'news/news');
