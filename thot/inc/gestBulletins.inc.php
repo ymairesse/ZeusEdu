2c182762b6554e5fef0f4407e84b25aaa560c2b1
Yves Mairesse <ymairesse@sio2.be>
2016-09-23 08:13:22 +0200

    Ajustement du sélecteur de classes

--- thot/inc/gestBulletins.inc.php
+++ thot/inc/gestBulletins.inc.php
@@ -13,7 +13,7 @@ $smarty->assign('classe', $classe);
 $listeEleves = $Ecole->listeEleves($classe, 'groupe');
 $smarty->assign('listeEleves', $listeEleves);
 $smarty->assign('listeClasses', $Ecole->listeGroupes());
-$smarty->assign('selecteur', 'selectClasse');
+$smarty->assign('selecteur', 'selecteurs/selectClassePost');
 
 if ($classe != null) {
     $smarty->assign('listeBulletinsEleves', $Thot->listeBulletinsEleves($classe));
