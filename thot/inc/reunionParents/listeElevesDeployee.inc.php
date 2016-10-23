70412c28cd5ff4529613f23ff8d13fed84394117
Yves Mairesse <ymairesse@sio2.be>
2016-10-23 23:39:58 +0200

    Rectification sur accolade manquante

--- thot/inc/reunionParents/listeElevesDeployee.inc.php
+++ thot/inc/reunionParents/listeElevesDeployee.inc.php
@@ -1,32 +1,34 @@
 <?php
-require_once("../../../config.inc.php");
+
+require_once '../../../config.inc.php';
 
 session_start();
 if (!(isset($_SESSION[APPLICATION]))) {
     die("<div class='alert alert-danger'>".RECONNECT.'</div>');
+}
 
-$classe = isset($_POST['classe'])?$_POST['classe']:Null;
-$date = isset($_POST['date'])?$_POST['date']:Null;
+$classe = isset($_POST['classe']) ? $_POST['classe'] : null;
+$date = isset($_POST['date']) ? $_POST['date'] : null;
 
-require_once(INSTALL_DIR.'/inc/classes/classApplication.inc.php');
+require_once INSTALL_DIR.'/inc/classes/classApplication.inc.php';
 $Application = new Application();
 
 $date = Application::dateMysql($date);
 
-require_once(INSTALL_DIR.'/inc/classes/classEcole.inc.php');
+require_once INSTALL_DIR.'/inc/classes/classEcole.inc.php';
 $Ecole = new Ecole();
 
-require_once(INSTALL_DIR.'/inc/classes/classThot.inc.php');
+require_once INSTALL_DIR.'/inc/classes/classThot.inc.php';
 $thot = new Thot();
 
-$listeEleves = $Ecole->listeEleves($classe,'groupe');
-$listeRV = $thot->getRVeleve(array_keys($listeEleves),$date);
+$listeEleves = $Ecole->listeEleves($classe, 'groupe');
+$listeRV = $thot->getRVeleve(array_keys($listeEleves), $date);
 
-require_once(INSTALL_DIR."/smarty/Smarty.class.php");
+require_once INSTALL_DIR.'/smarty/Smarty.class.php';
 $smarty = new Smarty();
-$smarty->template_dir = "../templates";
-$smarty->compile_dir = "../templates_c";
+$smarty->template_dir = '../templates';
+$smarty->compile_dir = '../templates_c';
 
-$smarty->assign('listeEleves',$listeEleves);
-$smarty->assign('listeRV',$listeRV);
+$smarty->assign('listeEleves', $listeEleves);
+$smarty->assign('listeRV', $listeRV);
 $smarty->display('../../templates/reunionParents/listeElevesDeployee.tpl');
