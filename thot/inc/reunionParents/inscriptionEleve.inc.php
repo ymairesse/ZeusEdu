37cd80a753943391cd5103aaa2b6136bb5db9e26
Yves Mairesse <ymairesse@sio2.be>
2016-11-14 10:34:15 +0100

    Paramétrage possible pour le nombre de RV par élève

--- thot/inc/reunionParents/inscriptionEleve.inc.php
+++ thot/inc/reunionParents/inscriptionEleve.inc.php
@@ -1,24 +1,29 @@
 <?php
-require_once("../../../config.inc.php");
+
+require_once '../../../config.inc.php';
 
 session_start();
 
-$id = isset($_POST['id'])?$_POST['id']:Null;
-$matricule = isset($_POST['matricule'])?$_POST['matricule']:Null;
-$acronyme = isset($_POST['acronyme'])?$_POST['acronyme']:Null;
-$date = isset($_POST['date'])?$_POST['date']:Null;
-$periode = isset($_POST['periode'])?$_POST['periode']:Null;
-$userName = isset($_POST['userName'])?$_POST['userName']:Null;
+$id = isset($_POST['id']) ? $_POST['id'] : null;
+$matricule = isset($_POST['matricule']) ? $_POST['matricule'] : null;
+$acronyme = isset($_POST['acronyme']) ? $_POST['acronyme'] : null;
+$date = isset($_POST['date']) ? $_POST['date'] : null;
+$periode = isset($_POST['periode']) ? $_POST['periode'] : null;
+$userName = isset($_POST['userName']) ? $_POST['userName'] : null;
 
-require_once(INSTALL_DIR.'/inc/classes/classApplication.inc.php');
+// nombre maximum de RV admissibles pour un élève
+DEFINE ("MAX", 3);
+
+require_once INSTALL_DIR.'/inc/classes/classApplication.inc.php';
 $Application = new Application();
 
-require_once(INSTALL_DIR.'/inc/classes/classThot.inc.php');
+require_once INSTALL_DIR.'/inc/classes/classThot.inc.php';
 $thot = new Thot();
 // max trois rendez-vous
-$resultat = $thot->inscriptionEleve($id, $matricule, 3, $userName);
+$resultat = $thot->inscriptionEleve($id, $matricule, MAX, $userName);
 // le cas échéant, supprimer de la liste d'attente
-if ($resultat > 0)
+if ($resultat > 0) {
     $thot->delListeAttenteProf($matricule, $acronyme, $date, $periode);
+}
 
 echo $resultat;
