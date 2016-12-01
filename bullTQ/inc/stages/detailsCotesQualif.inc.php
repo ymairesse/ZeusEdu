a3b6aa386cb666a6162a62bbfdd9709d0e6e812d
Yves Mairesse <ymairesse@sio2.be>
2016-12-01 20:49:17 +0100

    Rationnalisation de l'attribution des cotes de qualification

--- bullTQ/inc/stages/detailsCotesQualif.inc.php
+++ bullTQ/inc/stages/detailsCotesQualif.inc.php
@@ -18,32 +18,14 @@ $Ecole = new Ecole();
 
 $detailsEleve = $Ecole->detailsDeListeEleves($matricule);
 $detailsEleve = $detailsEleve[$matricule];
-$annee = substr($detailsEleve['groupe'], 0, 1);
 
-$legendes = array(
-    'E1' => 'Évaluation du stage 5e',
-    'E2' => 'Rapport de stage 5e',
-    'E3' => 'Évaluation du stage 6e',
-    'E4' => 'Rapport de stage 6e',
-    'JURY' => 'Jury',
-    'TOTAL' => 'Total',
-);
+$annee = $detailsEleve['annee'];
 
-$evaluations = $BullTQ->evalStagesEleve($matricule, $legendes);
-if ($evaluations != null)
+$qualification = $BullTQ->listeEpreuvesQualif();
+
+$evaluations = $BullTQ->evalStagesEleve($matricule, $qualification);
+if ($evaluations != null) {
     $evaluations = $evaluations[$matricule];
-
-// certains champs sont activés en 5e, d'autres en 6e
-$activations = array();
-$champs5 = array('E1', 'E2');
-$champs6 = array('E3', 'E4');
-foreach ($evaluations as $champ => $value) {
-    if ($annee == 5) {
-        $activations[$champ] = (in_array($champ, $champs5));
-    }
-    if ($annee == 6) {
-        $activations[$champ] = (in_array($champ, $champs6));
-    }
 }
 
 require_once INSTALL_DIR.'/smarty/Smarty.class.php';
@@ -51,8 +33,8 @@ $smarty = new Smarty();
 $smarty->template_dir = '../../templates';
 $smarty->compile_dir = '../../templates_c';
 
+$smarty->assign('annee', $annee);
 $smarty->assign('evaluations', $evaluations);
-$smarty->assign('activations', $activations);
-$smarty->assign('legendes', $legendes);
+$smarty->assign('qualification', $qualification);
 
 echo $smarty->fetch('stages/editEvaluations.tpl');
--- bullTQ/templates/stages/editEvaluations.tpl
+++ bullTQ/templates/stages/editEvaluations.tpl
@@ -1,17 +1,14 @@
 <form>
 
-    {foreach from=$evaluations key=champ item=value}
-
-    <div class="form-group">
-        <label for="{$champ}">{$legendes.$champ}</label>
-        <div class="input-group">
-            <input type="text" class="form-control" id="{$champ}" value="{$value}" {if !$activations[$champ]}disabled{/if}>
-            <span class="input-group-addon"><i class="fa {if !$activations.$champ}fa-ban{else}fa-check{/if}"></i>
-
-            </span>
+    {foreach from=$qualification item=epreuve}
+        {assign var=sigle value=$epreuve.sigle}
+        <div class="form-group">
+            <label for="{$sigle}">{$sigle}</label>
+            <div class="input-group">
+                <input type="text" class="form-control" id="{$sigle}" value="{$evaluations.$sigle}" {if ($annee != $epreuve.annee)} disabled{/if}>
+                <span class="input-group-addon"><i class="fa {if ($annee != $epreuve.annee)}fa-ban{else}fa-check{/if}"></i></span>
+            </div>
         </div>
-
-    </div>
 
     {/foreach}
 
