d528dea1106b9bf879bfdb2e496b85e975dd9e3e
Yves Mairesse <ymairesse@sio2.be>
2017-07-15 19:22:06 +0200

    Passage à jquery-3.2.1

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="../js/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="../js/jquery-3.2.1.min.js"></script>
 
 <script type="text/javascript" src="../js/jquery.validate.js"></script>
 <script type="text/javascript" src="../js/jquery.blockUI.js"></script>
