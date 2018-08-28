6fca4b808f43dea32a5fafbc3fceb479ea4120cc
Yves Mairesse <ymairesse@sio2.be>
2018-08-28 10:38:50 +0200

    Ajout de la librairie js-cookie

---
+++ js/jsCookie/Gruntfile.js
@@ -0,0 +1,198 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function (grunt) {
+
+	function encodingMiddleware(request, response, next) {
+		var url = require('url').parse(request.url, true, true);
+		var query = url.query;
+		var pathname = url.pathname;
+
+		if (pathname !== '/encoding') {
+			next();
+			return;
+		}
+
+		var cookieName = query.name;
+		var cookieValue = query.value;
+
+		response.setHeader('content-type', 'application/json');
+		response.end(JSON.stringify({
+			name: cookieName,
+			value: cookieValue
+		}));
+	}
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		qunit: {
+			all: {
+				options: {
+					urls: [
+						'http://127.0.0.1:9998/',
+						'http://127.0.0.1:9998/amd.html',
+						'http://127.0.0.1:9998/environment-with-amd-and-umd.html',
+						'http://127.0.0.1:9998/encoding.html?integration_baseurl=http://127.0.0.1:9998/'
+					]
+				}
+			},
+		},
+		nodeunit: {
+			all: 'test/node.js'
+		},
+		eslint: {
+			grunt: 'Gruntfile.js',
+			source: 'src/**/*.js',
+			tests: ['test/**/*.js', '!test/polyfill.js']
+		},
+		uglify: {
+			options: {
+				compress: {
+					unsafe: true
+				},
+				screwIE8: false,
+				banner: '/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.license %> */\n'
+			},
+			build: {
+				files: {
+					'build/js.cookie.min.js': 'src/js.cookie.js',
+					'build/js.cookie-<%= pkg.version %>.min.js': 'src/js.cookie.js'
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			files: '{src,test}/**/*.js',
+			tasks: 'default'
+		},
+		compare_size: {
+			files: [
+				'build/js.cookie-<%= pkg.version %>.min.js',
+				'src/js.cookie.js'
+			],
+			options: {
+				compress: {
+					gz: function (fileContents) {
+						return require('gzip-js').zip(fileContents, {}).length;
+					}
+				}
+			}
+		},
+		connect: {
+			'build-qunit': {
+				options: {
+					port: 9998,
+					base: ['.', 'test'],
+					middleware: function (connect, options, middlewares) {
+						middlewares.unshift(encodingMiddleware);
+						return middlewares;
+					}
+				}
+			},
+			'build-sauce': {
+				options: {
+					port: 9999,
+					base: ['.', 'test']
+				}
+			},
+			tests: {
+				options: {
+					port: 10000,
+					base: ['.', 'test'],
+					open: 'http://127.0.0.1:10000',
+					keepalive: true,
+					livereload: true,
+					middleware: function (connect, options, middlewares) {
+						middlewares.unshift(encodingMiddleware);
+						return middlewares;
+					}
+				}
+			}
+		},
+		'saucelabs-qunit': {
+			all: {
+				options: {
+					urls: ['http://127.0.0.1:9999'],
+					testname: 'Sauce Test for js-cookie',
+					build: process.env.TRAVIS_JOB_ID,
+					statusCheckAttempts: -1,
+					throttled: 3,
+					browsers: [
+						{
+							browserName: 'safari',
+							platform: 'OS X 10.11',
+							version: '10.0'
+						},
+						{
+							browserName: 'safari',
+							platform: 'OS X 10.12',
+							version: '10.1'
+						},
+						{
+							browserName: 'safari',
+							platform: 'OS X 10.13',
+							version: '11.0'
+						},
+						{
+							browserName: 'firefox',
+							platform: 'OS X 10.11',
+							version: '56.0'
+						},
+						{
+							browserName: 'chrome',
+							platform: 'OS X 10.10',
+							version: '61.0'
+						},
+						{
+							browserName: 'internet explorer',
+							platform: 'Windows 7',
+							version: '11.0'
+						},
+						{
+							browserName: 'internet explorer',
+							platform: 'Windows 7',
+							version: '10.0'
+						},
+						{
+							browserName: 'internet explorer',
+							platform: 'Windows 7',
+							version: '9.0'
+						},
+						{
+							browserName: 'internet explorer',
+							platform: 'Windows 7',
+							version: '8.0'
+						},
+						{
+							browserName: 'firefox',
+							platform: 'Linux',
+							version: '45.0'
+						},
+						{
+							browserName: 'chrome',
+							platform: 'Linux',
+							version: '48.0'
+						}
+					]
+				}
+			}
+		}
+	});
+
+	// Loading dependencies
+	for (var key in grunt.file.readJSON('package.json').devDependencies) {
+		if (key !== 'grunt' && key.indexOf('grunt') === 0) {
+			grunt.loadNpmTasks(key);
+		}
+	}
+
+	grunt.registerTask('saucelabs', ['connect:build-sauce', 'saucelabs-qunit']);
+	grunt.registerTask('test', ['uglify', 'eslint', 'connect:build-qunit', 'qunit', 'nodeunit']);
+
+	grunt.registerTask('dev', ['test', 'compare_size']);
+	grunt.registerTask('ci', ['test', 'saucelabs']);
+
+	grunt.registerTask('default', 'dev');
+};
---
+++ js/jsCookie/test/amd-config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+var require = {
+	paths: {
+		'qunit': [
+			'../node_modules/qunitjs/qunit/qunit'
+		]
+	}
+};
