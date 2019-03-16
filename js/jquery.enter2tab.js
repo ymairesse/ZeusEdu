9242b2a245d8b4af5a409620877ad8190b2d8bfc
Yves Mairesse <ymairesse@sio2.be>
2019-03-16 12:07:53 +0100

    Changement pour l'événement 'on'

--- js/jquery.enter2tab.js
+++ js/jquery.enter2tab.js
@@ -6,19 +6,19 @@ ARROW_RIGHT = 39;
 ARROW_DOWN = 40;
 ENTER = 13;
 ESC = 27;
-TAB = 9;	
+TAB = 9;
 
-	this.keypress(function(e){
+	this.on('keydown', function(e){
 		// get key pressed (charCode from Mozilla/Firefox and Opera / keyCode in IE)
 		var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
-		
+
 		if ((key == ENTER) || (key == ARROW_DOWN)) {
 			// get tabindex from which element keypressed
 			var ntabindex = parseInt($(this).attr("tabindex"));
 			$("[tabindex=" + eval(ntabindex+1) + "]").focus();
 			return false;
 		}
-		
+
 		if (key == ARROW_UP) {
 			var ntabindex = parseInt($(this).attr("tabindex"));
 			$("[tabindex=" + eval(ntabindex-1) + "]").focus();
